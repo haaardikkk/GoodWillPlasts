3f3dc8c7c448a5ace1efb6e38978d5a791f2a27c
Hardik <codesmith.hardik@outlook.com>
2025-10-08 18:09:33 +0530

    ✨ Updated contact form with EmailJS and fixed deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './',
   server: {
     host: "::",
     port: 8080,
+  },
+  build: {
+    outDir: 'dist',
   },
   plugins: [
     react(),
